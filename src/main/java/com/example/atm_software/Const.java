cdcc765ba946a8361b4be3b5466900b1c1f1657c
zemor <sergej606@mail.ru>
2024-03-16 00:11:33 +0300

    Соловьев

---
+++ src/main/java/com/example/atm_software/Const.java
@@ -0,0 +1,11 @@
+package com.example.atm_software;
+
+public class Const {
+    public static final String USER_TABLE = "users";
+    public static final String USER_ID = "userID";
+    public static final String USER_FIRSTNAME = "firstname";
+    public static final String USER_LASTNAME = "lastname";
+    public static final String USER_SURNAME = "surname";
+    public static final String USER_PHONENUMBER = "phoneNumber";
+    public static final String USER_PINKOD = "pinKod";
+}
