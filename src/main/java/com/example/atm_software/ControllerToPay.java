c32f389290a089a05bc2b0f6e0c164b257478456
lXvladXl <vl1234544@gmail.com>
2024-03-19 10:22:15 +0300

    3

--- src/main/java/com/example/atm_software/ControllerToPay.java
+++ src/main/java/com/example/atm_software/ControllerToPay.java
@@ -1,4 +1,30 @@
 package com.example.atm_software;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class ControllerToPay {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button ControllerToPayBank;
+
+    @FXML
+    private Button ControllerToPayCard;
+
+    @FXML
+    private Button ControllerToPayMComm;
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
