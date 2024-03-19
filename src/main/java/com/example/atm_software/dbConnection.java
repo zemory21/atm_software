53352e6b76b9b5f5ee80f3a4c008838a784f0ff5
lXvladXl <vl1234544@gmail.com>
2024-03-19 09:41:26 +0300

    3

--- src/main/java/com/example/atm_software/dbConnection.java
+++ src/main/java/com/example/atm_software/dbConnection.java
@@ -8,9 +8,46 @@ public class dbConnection {
     static final String password = "12341324";
 
     public static void main(String[] args) {
-        try (Connection conn = DriverManager.getConnection(url, user, password)) {
-            var stm = conn.createStatement();
-            stm.executeUpdate("insert into users (lastname, firstname, surname, phoneNumber, pinKod)"+"values('Григорьев', 'Игорь', 'Витальевич', '89016995224', '0234')");
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            if (!conn.isClosed()) {
+                System.out.println("Норм");
+            } else {
+                System.out.println("плохо");
+            }
+            String request = "INSERT INTO" + Const.USER_TABLE + "(lastname, firstname, surname," +
+                    " phoneNumber, pinKod)" + "VALUES(?,?,?,?,?)";
+            var stm = conn.prepareStatement(request);
+            stm.setString(1, Const.USER_LASTNAME);
+            stm.setString(2, Const.USER_FIRSTNAME);
+            stm.setString(3, Const.USER_SURNAME);
+            stm.setString(4, Const.USER_PHONENUMBER);
+            stm.setString(5, Const.USER_PINKOD);
+            stm.executeUpdate();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public void signUpUser(String lastname, String firstname, String surname, String phoneNumber, String pinKod) {
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            if (!conn.isClosed()) {
+                System.out.println("Норм");
+            } else {
+                System.out.println("плохо");
+            }
+            String request = "INSERT INTO" + Const.USER_TABLE + "(lastname, firstname, surname," +
+                    " phoneNumber, pinKod)" + "VALUES(?,?,?,?,?)";
+            var stm = conn.prepareStatement(request);
+            stm.setString(1, lastname);
+            stm.setString(2, firstname);
+            stm.setString(3, surname);
+            stm.setString(4, phoneNumber);
+            stm.setString(5, pinKod);
+            stm.executeUpdate();
+            conn.close();
         } catch (SQLException e) {
             System.out.println("Error: " + e.getMessage());
         }
