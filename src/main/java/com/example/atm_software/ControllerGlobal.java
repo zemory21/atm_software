1e7d31c9fad67f5cd3521a2a000d90576650157b
lXvladXl <vl1234544@gmail.com>
2024-03-19 08:59:28 +0300

    3

---
+++ src/main/java/com/example/atm_software/ControllerGlobal.java
@@ -0,0 +1,39 @@
+package com.example.atm_software;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class ControllerGlobal {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label ControllerSingInEnter;
+
+    @FXML
+    private Button ControllrGlobalHistory;
+
+    @FXML
+    private Button ControllrGlobalInsert;
+
+    @FXML
+    private Button ControllrGlobalSendAnotherBank;
+
+    @FXML
+    private Button ControllrGlobalTakeOff;
+
+    @FXML
+    private Button ControllrGlobalToPay;
+
+    @FXML
+    void initialize() {
+    }
+
+}
