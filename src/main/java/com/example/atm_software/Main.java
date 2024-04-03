2814cc9e9d807ba03cf213669d02bac3a8cdb49e
Sergey Solovevy <sergej606@mail.ru>
2024-04-03 20:12:15 +0300

    объединил две ветки

--- src/main/java/com/example/atm_software/Main.java
+++ src/main/java/com/example/atm_software/Main.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("SignIn.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("SignIn.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 450);
         stage.setTitle("ATM");
         stage.setScene(scene);
